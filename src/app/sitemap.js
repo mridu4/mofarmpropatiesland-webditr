cbec3ded24eef048d91ed5ff963fd8fc8bbf9e0d
mridu4 <immanueljuma@gmail.com>
2026-02-18 15:07:03 +0300

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://mofarmlandproperties.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mofarmlandproperties.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mofarmlandproperties.com/properties',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mofarmlandproperties.com/contact',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://mofarmlandproperties.com/how-to-buy',
+      lastModified: new Date(),
+    },
+  ]
+}
